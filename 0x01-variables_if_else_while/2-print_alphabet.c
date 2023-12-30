87de1e668ad0f622cfaecd34537644a7d2ab77c3
Ahmed-Mohamed-Fahmey <ahmed.m.fahmy303@gmail.com>
2023-12-30 05:12:42 +0200

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main -Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+    int c = 97;
+
+    while (c < 123)
+    {
+        putchar(c);
+        c++;
+    }
+
+    return (0);
+}
