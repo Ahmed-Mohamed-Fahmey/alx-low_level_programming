2ab6dfe23cffd270d5e55bb67964fcb04cbbe1bf
Ahmed-Mohamed-Fahmey <ahmed.m.fahmy303@gmail.com>
2023-12-30 07:32:29 +0200

    new

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		putchar(a + '0');
+		if (a == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
