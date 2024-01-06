9139610584e3884873002ca7585275031bd3e594
Ahmed-Mohamed-Fahmey <ahmed.m.fahmy303@gmail.com>
2024-01-06 10:13:51 +0200

    new

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints from any integer number to 98
+ * @n: int number to start printing from
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		printf("%d ,", n);
+		n++;
+	}
+	else
+	{
+		printf("%d ,", n);
+		n--;
+	}
+}
