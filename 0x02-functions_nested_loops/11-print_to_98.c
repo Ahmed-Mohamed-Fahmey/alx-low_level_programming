99da823b88c97e2f6ccaa08c319b0b6d187ca897
Ahmed-Mohamed-Fahmey <ahmed.m.fahmy303@gmail.com>
2024-01-06 10:17:48 +0200

    new

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,12 +10,18 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		printf("%d ,", n);
-		n++;
+		while (n <= 98)
+		{
+			printf("%d ,", n);
+			n++;
+		}
 	}
 	else
 	{
-		printf("%d ,", n);
-		n--;
+		while (n > 98)
+		{
+			printf("%d ,", n);
+			n--;
+		}
 	}
 }
