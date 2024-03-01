4f9189b88b31fdfe4ed6624f9262a27b1bb542bb
Ahmed-Mohamed-Fahmey <ahmed.m.fahmy303@gmail.com>
2024-03-01 08:26:36 +0200

    new

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,24 +14,17 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-		{
 			printf("FizzBuzz");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz");
-		}
 		else
-		{
-			printf("%d", i);
-		}
-		putchar(' ');
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
 	}
 
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
