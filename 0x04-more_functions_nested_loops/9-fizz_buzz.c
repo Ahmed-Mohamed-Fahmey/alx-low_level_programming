115982dfa759733c430aed5125d3862d8f251f49
Ahmed-Mohamed-Fahmey <ahmed.m.fahmy303@gmail.com>
2024-03-01 08:21:31 +0200

    new

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,6 +2,9 @@
 
 /**
  * main - Entry point
+ * * Description: prints the numbers 1 - 100 w/
+ * Fizz for multiples of 3, Buzz for multiples of 5,
+ * and FizzBuzz for multiples of both
  * Return: 0
  */
 int main(void)
